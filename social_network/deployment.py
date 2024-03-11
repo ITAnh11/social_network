6e8a4bb4149aea827039a3648c399e263fb8d3a7
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2024-03-12 00:06:14 +0700

    change files for azure - test 1

--- social_network/deployment.py
+++ social_network/deployment.py
@@ -1,7 +1,8 @@
 import os
-from .settings import *
+from .settings import *  # noqa: F403
 from .settings import BASE_DIR
 
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
