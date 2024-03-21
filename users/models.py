50f87dd3ba58379c3a51abbdedc40a1b4058c3d0
Tuan Anh <buianhkc112004@gmail.com>
2024-03-22 00:13:09 +0700

    add mediaprofile models for save avatar and background of user

--- users/models.py
+++ users/models.py
@@ -14,15 +14,22 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+def media_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/avatars/<filename>
+    return 'users/userprofile_{0}/{1}'.format(instance.user_id.id,filename)
+
 class UserProfile(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, null=False)
     last_name = models.CharField(max_length=255, null=False)
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, null=False)
     address = models.CharField(max_length=255, blank=True)
-    avatar_url = models.ImageField(upload_to='avatars/', blank=True)
-    background_url = models.ImageField(upload_to='backgrounds/', blank=True)
     bio = models.TextField(blank=True)
     school = models.CharField(max_length=255, blank=True)
     work = models.CharField(max_length=255, blank=True)
+
+class MediaProfile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    avatar= models.ImageField(upload_to=media_directory_path, blank=True)
+    background = models.ImageField(upload_to=media_directory_path, blank=True)
