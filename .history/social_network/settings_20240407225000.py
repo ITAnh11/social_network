6da73b247d4f1b46db6afbbc2c6dc419a8e0b7fb
Nguyen Quoc Vuong <nguyenquocvuong161104@gmail.com>
2024-04-10 22:36:54 +0700

    modife__requirment

--- .history/social_network/settings_20240407225000.py
+++ .history/social_network/settings_20240407225000.py
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'social_network',
         'USER': 'admin',
-        'PASSWORD': 'abc123',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
