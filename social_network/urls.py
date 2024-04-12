2e93bb7adcb1db23fba3c4e0e0cb171892f6e979
MinhTuan <minhtuan@gmail.com>
2024-04-12 23:57:16 +0700

    navbar upload but not good

--- social_network/urls.py
+++ social_network/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('mess/', include('mess.urls')),
     path("chat/", include("chat.urls")),
     path('comments/', include('comments.urls')),
+    path('navbar/', include('navbar.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
