6568fc01ea6206b9b70aca053b3750f7eced0d1a
DAOGIANGAN <125682086+DAOGIANGAN@users.noreply.github.com>
2024-04-13 22:49:22 +0700

    update_friend/page

--- friends/static/friends/js/friend.js
+++ friends/static/friends/js/friend.js
@@ -1,10 +1,24 @@
-//loại bỏ lời mời kết bạn và  lời gợi ý kết bạn
-var button2 = document.querySelectorAll('.button2');
-button2.forEach(function(button){
-    button.onclick = function(){
-        button.parentElement.parentElement.remove();
+
+
+//Xử lí denine button
+function denine_button(event){
+    var a = event.target.parentNode.parentNode.id;
+
+    const formdata = new FormData();
+    formdata.append("st", "denined");
+    formdata.append("id", a);
+    url = "/friends/denine_friendrequest/";
+
+    fetch(url,{
+        method:'POST',
+        body: formdata,
+    })
+
+
+    if(event.target.textContent === "Từ chối") {
+        event.target.textContent = "Đã từ chối";
     }
-})
+}
 
 //xử lí accept button
 function accept_button(event){
@@ -14,7 +28,7 @@ function accept_button(event){
     }
     var a = event.target.parentNode.parentNode.id;
 
-    const formdata = new FormData();
+    var formdata = new FormData();
 
     formdata.append("st", "accepted");
     formdata.append("id", a);
@@ -22,10 +36,6 @@ function accept_button(event){
     fetch(url,{
         method:'POST',
         body: formdata,
-    })
-    .then(response =>response.json())
-    .then(data =>{
-        console.log(data);
     })
 }
 
@@ -38,22 +48,24 @@ function addfriend() {
     .then(data => {
         console.log(data);
         data.data.forEach(function(request){
-            var a = `<div class="card1" id="${request.friend_request_received.id}">
-            <div class="card1-img">
-                <img style="object-fit: cover;width: 100%;height: 100%;" src="${request.friend_request_profile.avatar}" alt="Card Image" >
-            </div>
-            <div class="card1-content">
-                <h3>${request.friend_request_profile.name}</h3>
-            </div>
-            <div class="card1-button">
-                <button class="button1" onclick="accept_button(event)">Xác nhận</button>
-                <button class="button2">Xóa</button>
-            </div>
-        </div>`;
-        var newDiv = document.createElement("div");
-        newDiv.innerHTML = a;
-
-        request_list.appendChild(newDiv);
+            if(request.friend_request_received.status === "pending"){
+                var a = `<div class="card1" id="${request.friend_request_received.id}">
+                <div class="card1-img">
+                    <img style="object-fit: cover;width: 100%;height: 100%;" src="${request.friend_request_profile.avatar}" alt="Card Image" >
+                </div>
+                <div class="card1-content">
+                    <h3>${request.friend_request_profile.name}</h3>
+                </div>
+                <div class="card1-button">
+                    <button class="button1" onclick="accept_button(event)">Xác nhận</button>
+                    <button class="button2" onclick="denine_button(event)">Từ chối</button>
+                </div>
+            </div>`;
+            var newDiv = document.createElement("div");
+            newDiv.innerHTML = a;
+    
+            request_list.appendChild(newDiv);
+            }
         })
     })
 }
