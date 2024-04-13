4cf26e1f461377f3ee6553586fbfd5b146449eba
MinhTuan <minhtuan@gmail.com>
2024-04-14 00:44:02 +0700

    navbar update

--- navbar/static/navbar/js/navbar.js
+++ navbar/static/navbar/js/navbar.js
@@ -1,3 +1,4 @@
+/* user-settings */
 var userSettings = document.querySelector(".user-settings");
 var darkBtn = document.getElementById("dark-button");
 var LoadMoreBackground =document.querySelector(".btn-LoadMore");
@@ -13,3 +14,14 @@ var LoadMoreBackground =document.querySelector(".btn-LoadMore");
         function LoadMoreToggle(){
             LoadMoreBackground.classList.toggle("loadMoreToggle");
         };
+
+/* change-color */
+var currentUrl = window.location.href;
+if (currentUrl.includes("homepage/index.html")) {
+    document.getElementById("home").classList.add("active");
+} else if (currentUrl.includes("friends/friend.html")) {
+    document.getElementById("friend").classList.add("active");
+} else if (currentUrl.includes("userprofiles/profile.html")) {
+    document.getElementById("home").classList.remove("active");
+    document.getElementById("friend").classList.remove("active");
+}
