67bd669f12acfe7f29fa6f818b53fb585c303947
Tuan Anh <buianhkc112004@gmail.com>
2024-04-15 18:25:10 +0700

    change some field comments

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,36 @@
+# from django_mongoengine import fields, Document, EmbeddedDocument
+# from mongoengine.fields import EmbeddedDocumentField
+
+# # class Comments(Document):
+# #     id = fields.SequenceField(primary_key=True)
+# #     user_id = fields.IntField()
+# #     content = fields.StringField()
+# #     to_posts_id = fields.IntField()
+# #     to_comment_id = fields.IntField()
+# #     created_at = fields.DateTimeField()
+# #     updated_at = fields.DateTimeField()
+    
+# #     meta = {
+# #         'db': 'social_network',
+# #         'collection': 'comments',
+# #         'indexes': [
+# #             'to_posts_id',
+# #             'to_comment_id'
+# #         ]
+# #     }
+    
 from django_mongoengine import fields, Document, EmbeddedDocument
 from mongoengine.fields import EmbeddedDocumentField
 
+
+class UserComment(EmbeddedDocument):
+    id = fields.IntField()
+    name = fields.StringField()
+    avatar = fields.StringField()
+    
 class Comments(Document):
     id = fields.SequenceField(primary_key=True)
-    user_id = fields.IntField()
+    user = EmbeddedDocumentField(UserComment)
     content = fields.StringField()
     to_posts_id = fields.IntField()
     to_comment_id = fields.IntField()
@@ -18,31 +45,4 @@ class Comments(Document):
             'to_comment_id'
         ]
     }
-    
-# from django_mongoengine import fields, Document, EmbeddedDocument
-# from mongoengine.fields import EmbeddedDocumentField
-
-
-# class User(EmbeddedDocument):
-#     id = fields.IntField()
-#     name = fields.StringField()
-#     avatar = fields.StringField()
-    
-# class Comments(Document):
-#     id = fields.SequenceField(primary_key=True)
-#     user = EmbeddedDocumentField(User)
-#     content = fields.StringField()
-#     to_posts_id = fields.IntField()
-#     to_comment_id = fields.IntField()
-#     created_at = fields.DateTimeField()
-#     updated_at = fields.DateTimeField()
-    
-#     meta = {
-#         'db': 'social_network',
-#         'collection': 'comments',
-#         'indexes': [
-#             'to_posts_id',
-#             'to_comment_id'
-#         ]
-#     }
     
