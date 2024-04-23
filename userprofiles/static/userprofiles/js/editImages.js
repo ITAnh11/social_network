18787fad3cbadf31d97b15f484c8bc2876ab9283
MinhTuan <tuanmt2912@gmail.com>
2024-04-24 01:11:14 +0700

    upload images commit

---
+++ userprofiles/static/userprofiles/js/editImages.js
@@ -0,0 +1,26 @@
+// Lắng nghe sự kiện khi người dùng chọn file
+document.getElementById('upload').addEventListener('change', function(event) {
+    // Lấy ra file đã chọn
+    const file = event.target.files[0];
+
+    // Kiểm tra xem file có phải là ảnh không
+    if (file && file.type.startsWith('image')) {
+        // Tạo đối tượng FileReader để đọc file ảnh
+        const reader = new FileReader();
+
+        // Lắng nghe sự kiện khi FileReader hoàn thành việc đọc file
+        reader.onload = function(e) {
+            // Hiển thị ảnh đã chọn trong thẻ img
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            document.getElementById('avt-preview').innerHTML = '';
+            document.getElementById('avt-preview').appendChild(img);
+
+            document.getElementById('cover-preview').innerHTML = '';
+            document.getElementById('cover-preview').appendChild(img);
+        };
+
+        // Đọc file ảnh
+        reader.readAsDataURL(file);
+    }
+});
