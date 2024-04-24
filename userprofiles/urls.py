1569eb964f358bdb7063fc769cc18e3c3f1fe788
MinhTuan <tuanmt2912@gmail.com>
2024-04-25 01:14:42 +0700

    upload avt and cover

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import ProfileView, EditImages, EditProfileView, EditStoryView, ListFriendsView, SetUserProfileView, SetImageProfileView, GetProfileView, GetPostsView, GetUserProfileBasicView
-
+from .views import ProfileView, ListFriendsView, SetUserProfileView, SetImageProfileView, GetProfileView, GetPostsView, GetUserProfileBasicView
+from .viewsEdit import EditImagePage, EditAvatarView, EditCoverView, EditProfileView, EditStoryView
 from chat.views import ProfileDetail
 app_name = 'userprofiles'
 urlpatterns = [
         path('', ProfileView.as_view(), name='profile'),
-        path('editImages/', EditImages.as_view(), name='editImages'),
+        path('editImagesPage/', EditImagePage.as_view(), name='editImagesPage'),
+        path('editAvatar/', EditAvatarView.as_view(), name='editAvatar'),
+        path('editCover/', EditCoverView.as_view(), name="editCover"),
         path('editProfile/', EditProfileView.as_view(), name='editProfile'),
         path('editStory/', EditStoryView.as_view(), name='editStory'),
         path('listFriends/', ListFriendsView.as_view(), name='listFriends'),
