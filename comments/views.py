244fd9f4cef1f803bd13f74b0500f768864ba48f
Tuan Anh <buianhkc112004@gmail.com>
2024-04-25 18:15:01 +0700

    save avatar to localstorage after set avatar

--- comments/views.py
+++ comments/views.py
@@ -71,12 +71,10 @@ class GetCommentsForComment(APIView):
 
 class CreateComment(APIView):
     
-    def createUserBasicInfo(self, request):
-        user = json.loads(request.data.get('user'))
-        
-        return UserBasicInfo(id=user.get('id'), 
-                           name=user.get('name'), 
-                           avatar=user.get('avatar'))
+    def createUserBasicInfo(self, request):        
+        return UserBasicInfo(id=request.data.get('user_id'), 
+                           name=request.data.get('user_name'), 
+                           avatar=request.data.get('user_avatar'))
     
     def createComment(self, request):
         return Comments(to_posts_id=request.data.get('to_posts_id'), 
