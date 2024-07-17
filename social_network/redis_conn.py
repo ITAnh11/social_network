0e61dd23e0c866c20a6ad09a1cd4d3aa4c4bb4a0
Tuan Anh <buianhkc112004@gmail.com>
2024-07-17 23:47:53 +0700

    .

--- social_network/redis_conn.py
+++ social_network/redis_conn.py
@@ -1,4 +1,5 @@
 import redis
 from django.conf import settings
 
-redis_server = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+# redis_server = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+redis_server = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
