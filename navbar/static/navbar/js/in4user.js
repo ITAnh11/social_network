13ee722293ed9269d677075cdd33b96de5497d8b
Tuan Anh <buianhkc112004@gmail.com>
2024-07-17 22:46:40 +0700

    .

--- navbar/static/navbar/js/in4user.js
+++ navbar/static/navbar/js/in4user.js
@@ -4,24 +4,6 @@ let USER_ID = "";
 let USER_NAME = "";
 let USER_AVATAR = "";
 
-function get_user_profile() {
-    fetch(api_get_profile_basic)
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            USER_ID = data['id'];
-            USER_NAME = data['name'];
-            USER_AVATAR = data['avatar'];
-            setUserProfileForNavbar();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-}
-
-get_user_profile();
-
 function setUserProfileForNavbar() {
   var userName1 = document.getElementById('userName1');
   userName1.innerHTML += `<h4>${USER_NAME}</h4>`;
@@ -31,3 +13,19 @@ function setUserProfileForNavbar() {
     profileImage[i].innerHTML += `<img src ="${USER_AVATAR}" alt="avatarIcon">`;
   }
 }
+
+console.log("get_user_profile basic...");
+fetch(api_get_profile_basic)
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        USER_ID = data['id'];
+        USER_NAME = data['name'];
+        USER_AVATAR = data['avatar'];
+    })
+    .then(() => {
+        setUserProfileForNavbar();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
